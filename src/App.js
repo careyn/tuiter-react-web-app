f5b133441c58307cb36a6663e19fa20faedb8d4e
careyn <carey.n@northeastern.edu>
2022-11-05 20:47:54 -0400

    test

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
                  element={<HelloWorld/>}/>
           <Route path="/tuiter/*"
                  element={<Tuiter/>}/>
+          <Route path="/tuiter"
+                 element={<Tuiter/>}/>
         </Routes>
       </div>
     </BrowserRouter>
