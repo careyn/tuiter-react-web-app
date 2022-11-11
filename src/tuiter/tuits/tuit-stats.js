848d025d9ef51485de4c634bd9115246d235baa8
careyn <carey.n@northeastern.edu>
2022-11-11 13:37:31 -0500

    adding likes

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -2,15 +2,22 @@
 
 import React from "react";
 import '../index.css';
+import {useDispatch} from "react-redux";
+import {updateTuitThunk} from "../../services/tuits-thunks";
 
 const TuitStats = ({tuit}) => {
- let likedColor = tuit.liked ?  {color:'red'} : undefined
- let likedFill = tuit.liked ?  'fa-solid fa-heart mt-1' : 'fa-regular fa-heart mt-1'
+ const dispatch = useDispatch();
  return(
     <div className='wd-icons'>
         <i class="fa-regular fa-comment mt-1"></i> {tuit.replies}
         <i class="fa-solid fa-retweet mt-1"></i> {tuit.retuits}
-        <i style={likedColor} class={likedFill}></i> {tuit.likes}
+        <div>
+            <i onClick={() => dispatch(updateTuitThunk({
+            ...tuit,
+            likes: tuit.likes + 1
+            }))} className="bi bi-heart-fill me-2 text-danger"></i>
+            {tuit.likes}
+        </div>
         <i class="fa-solid fa-share-nodes mt-1"></i>
     </div>
  );
